3ecddf03eaae5c56249781b9103d8d354e22108a
serhii.berehovyi <serhii.berehovyi@gmail.com>
2022-12-06 19:24:25 +0100

    Initial Commit

---
+++ SimplyToDoList/SimplyToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyToDoListApp.swift
+//  SimplyToDoList
+//
+//  Created by Serhii Berehovyi on 06.12.22.
+//
+
+import SwiftUI
+
+@main
+struct SimplyToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
